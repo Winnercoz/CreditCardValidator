db97d1e2690799d2b807b2f19bcdbbcf12e8df89
[winnercoz] <[winnercoz340@gmail.com]>
2019-07-26 16:11:56 -0700

    have pushed my last push for this project and every file has been added including the javascript aspect

--- script/mypaycard.js
+++ script/mypaycard.js
@@ -1,5 +1,9 @@
 const submitButton = document.querySelector('[data-input-num]');
 
+function myfun(){
+    document.querySelector('#cardType').style.display = none;
+}
+
 const validate = ()=>{
     const dataCardNumber = document.getElementById('cardnumber').value;
     const firstDigit = dataCardNumber.toString()[0];
@@ -10,7 +14,7 @@ const validate = ()=>{
         if(dataCardNumber.length === 16){
             //IF THEY ARE 16 DIGITS ,LET CHECK FOR CARD TYPE
             if(firstDigit === '5'){
-                alert('THIS IS A VALID MASTER CARD, YOU CAN NOW PROCEED');
+                
             }
              else{
                  alert('THIS IS A VALID VISA CARD, YOU CAN NOW PROCEED')
